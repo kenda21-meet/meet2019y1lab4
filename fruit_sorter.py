910d70dd3b6a2911fbbe5ca058acc320a3a176bb
kenda21-meet <kenda21@meet.mit.edu>
2019-07-24 07:35:54 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,9 @@
+fruit=input('choose a kind of fruit')
+if (fruit=='apples'):
+    print('apples in bin 1')
+elif (fruit=='oranges'):
+    print('oranges in bin 2')
+elif (fruit=='bananas'):
+    print('bananas in bin 3')
+else:
+    print('Error!I do not recognize this fruit!')
