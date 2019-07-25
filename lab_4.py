e4cdcb543bdc985d1ef2b9d2f67e23001d564f3e
kenda21-meet <kenda21@meet.mit.edu>
2019-07-25 12:47:50 -0400

    Stopping here

---
+++ lab_4.py
@@ -0,0 +1,9 @@
+fruit=input("what kind of fruit do you want?")
+if (fruit=="apples"):
+    print("apples in bin 1")
+elif (fruit=="oranges"):
+    print("oranges in bin 2")
+elif (fruit=="bananas"):
+    print("bananas in bin 3")
+else:
+    print("ERROR!")
